5f69b87e00f8af2505859211343add4240a4cc2d
Matt <mattmaliaros@gmail.com>
2019-08-19 16:54:21 -0400

    heroku

--- connection.js
+++ connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "password", // add your local password here.
-    database: "notetaker_db" // add your db name here
+    user: "zj0j4fzc83ugmgt8",
+    password: "u35ujtuzw9irrlwe", // add your local password here.
+    database: "cv921igp88038d8d" // add your db name here
   });
 }
 
